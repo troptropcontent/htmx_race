ef874c5b66113ca2cc12e201033d94ba56dfd368
Tom Ecrepont <tomecrepont@gmail.com>
2023-12-03 17:43:00 +0000

    create a seed

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,3 +1,4 @@
 class Race < ApplicationRecord
-  belongs_to :user
+  has_and_belongs_to_many :owners, join_table: :race_owners, class_name: "User", association_foreign_key: :user_id
+  has_many :participants, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+DEFAULT_PASSWORD = "test123".freeze
+
+
+User.destroy_all
+Race.destroy_all
+
+main_user = User.create(email: "tom@example.com", password: DEFAULT_PASSWORD)
+10.times do |n|
+    user_number = (n+1).to_s.rjust(2, "0")
+    User.create(email: "user#{user_number}@example.com", password: DEFAULT_PASSWORD)
+end
+
+5.times do |n|
+    race_name ="Course " + "#{n+1}".rjust(2, "0")
+    race = main_user.owned_races.create(name: race_name)
+    User.where.not(id: race.owners.ids).all.sample(rand(1..4)).each do |user|
+        race.participants.create(user: user)
+    end
+end
