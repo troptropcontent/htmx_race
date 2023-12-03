68588c6742640418cd8d5adc481f284c626a191e
Tom Ecrepont <tomecrepont@gmail.com>
2023-12-03 16:59:38 +0000

    create the race model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_165429) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_03_165914) do
+  create_table "races", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_races_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_03_165429) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "races", "users"
 end
