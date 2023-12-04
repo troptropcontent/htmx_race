7fe9e8d9adb0dd2f97209559c604bf201b1b58fc
Tom Ecrepont <tomecrepont@gmail.com>
2023-12-04 09:10:46 +0000

    first draft of controller tests

--- test/controllers/races_controller_test.rb
+++ test/controllers/races_controller_test.rb
@@ -1,11 +1,13 @@
 require "test_helper"
-
 class RacesControllerTest < ActionDispatch::IntegrationTest
-  test "when the user is not logged in" do
-    test "it should redirect to sign in" do
-      puts users
-      get races_url
-      assert_response :success
-    end
+  include Devise::Test::IntegrationHelpers
+  test "when the user is not logged in it redirect to the sign in page" do
+    get races_url
+    assert_redirected_to new_user_session_url
+  end
+  test "when the user is logged it works successfully" do
+    sign_in users(:user_1)
+    get races_url
+    assert_response :success
   end
 end
